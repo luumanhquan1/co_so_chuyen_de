f41bcf063bcefdbdd93b3f963e371533720b577f
DESKTOP-88FAOV3\EdsoLabs <>
2022-01-05 09:36:27 +0700

    day code lai

--- lib/presentation/tabbar_screen/ui/tabbar_item.dart
+++ lib/presentation/tabbar_screen/ui/tabbar_item.dart
@@ -1,4 +1,6 @@
 import 'package:ccvc_mobile/generated/l10n.dart';
+import 'package:ccvc_mobile/presentation/bao_chi_mang_xa_hoi_screen/tabbar/ui/tabbar_newspaper.dart';
+import 'package:ccvc_mobile/presentation/calender_work/main_tabbar_calender_work.dart';
 import 'package:ccvc_mobile/utils/constants/image_asset.dart';
 
 import 'package:flutter/cupertino.dart';
@@ -52,7 +54,7 @@ extension TabbarEnum on TabBarType {
           backgroundColor: Colors.blue,
         );
       case TabBarType.calendarWork:
-        return MainTabarCalenderWork();
+        return const MainTabarCalenderWork();
       case TabBarType.internalInteraction:
         return const Scaffold(
           backgroundColor: Colors.cyanAccent,
