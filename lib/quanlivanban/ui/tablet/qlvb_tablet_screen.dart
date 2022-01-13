7688a8ddd9d3c196a7e6f3dd379fb412d4240b73
HoaVu <hoavd@edsolabs.com>
2022-01-13 13:31:48 +0700

    fix

--- lib/quanlivanban/ui/tablet/qlvb_tablet_screen.dart
+++ lib/quanlivanban/ui/tablet/qlvb_tablet_screen.dart
@@ -28,74 +28,108 @@ class _QLVBScreenTabletState extends State<QLVBScreenTablet> {
       appBar: AppBarDefaultBack(
         S.current.thong_tin_chung,
       ),
-      body: SingleChildScrollView(
-        physics: const AlwaysScrollableScrollPhysics(),
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 30,vertical: 20),
-          color: bgQLVBTablet,
-          child: Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  border: Border.all(color: borderColor.withOpacity(0.5)),
-                ),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: StreamBuilder<DocumentDashboardModel>(
-                        stream: qlvbCubit.getVbDen,
-                        builder: (context, snapshot) {
-                          final dataVBDen =
-                              snapshot.data ?? DocumentDashboardModel();
-                          return CommonInformationTablet(
-                            documentDashboardModel: dataVBDen,
-                            isVbDen: true,
-                            title: S.current.document_incoming,
-                          );
-                        },
-                      ),
-                    ),
-                    Expanded(
-                      child: StreamBuilder<DocumentDashboardModel>(
-                        stream: qlvbCubit.getVbDi,
-                        builder: (context, snapshot) {
-                          final dataVBDi =
-                              snapshot.data ?? DocumentDashboardModel();
-                          return CommonInformationTablet(
-                            documentDashboardModel: dataVBDi,
-                            isVbDen: false,
-                            title: S.current.document_out_going,
-                          );
-                        },
-                      ),
-                    ),
-
-                  ],
-                ),
+      body: Container(
+        color: bgQLVBTablet,
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(color: borderColor.withOpacity(0.5)),
               ),
-              DefaultTabController(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: StreamBuilder<DocumentDashboardModel>(
+                      stream: qlvbCubit.getVbDen,
+                      builder: (context, snapshot) {
+                        final dataVBDen =
+                            snapshot.data ?? DocumentDashboardModel();
+                        return CommonInformationTablet(
+                          documentDashboardModel: dataVBDen,
+                          isVbDen: true,
+                          title: S.current.document_incoming,
+                        );
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: StreamBuilder<DocumentDashboardModel>(
+                      stream: qlvbCubit.getVbDi,
+                      builder: (context, snapshot) {
+                        final dataVBDi =
+                            snapshot.data ?? DocumentDashboardModel();
+                        return CommonInformationTablet(
+                          documentDashboardModel: dataVBDi,
+                          isVbDen: false,
+                          title: S.current.document_out_going,
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 300,
+              color: bgQLVBTablet,
+              child: DefaultTabController(
                 length: 2,
                 child: Scaffold(
-                  appBar: AppBar(
-                    bottom: const TabBar(
-                      tabs: [
-                        Text('tab1'),
-                        Text('tab2'),
+                  appBar: PreferredSize(
+                    preferredSize: const Size.fromHeight(40.0),
+                    child: AppBar(
+                      backgroundColor: bgQLVBTablet,
+                      elevation: 0.0,
+                      bottom: TabBar(
+                        labelColor: labelColor,
+                        unselectedLabelColor: unselectedLabelColor,
+                        indicatorColor: indicatorColor,
+                        tabs: [
+                          Text(S.current.danh_sach_van_ban_den),
+                          Text(S.current.danh_sach_van_ban_di),
+                        ],
+                      ),
+                    ),
+                  ),
+                  body: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          height: 30,
+                          width: 80,
+                          color: Colors.red,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                         SizedBox(
+                          height: 100,
+                          child: const TabBarView(
+                            children: [
+                              Center(
+                                child: Text('Tab1'),
+                              ),
+                              Center(
+                                child: Text('Tab2'),
+                              ),
+                            ],
+                          ),
+                        ),
                       ],
                     ),
-
-                  ),
-                  body:const TabBarView(
-                    children: [
-                      Center(child: Text('Tab1'),),
-                      Center(child: Text('Tab2'),),
-                    ],
                   ),
                 ),
-              )
-            ],
-          ),
+              ),
+            ),
+          ],
         ),
       ),
     );
