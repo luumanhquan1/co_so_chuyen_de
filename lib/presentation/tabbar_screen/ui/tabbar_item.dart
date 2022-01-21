5bca6ed7d690cbce5a94e7429890654e0c2f6caa
luchn <hangocluc2001@gmail.com>
2022-01-21 10:36:20 +0700

    update code

--- lib/presentation/tabbar_screen/ui/tabbar_item.dart
+++ lib/presentation/tabbar_screen/ui/tabbar_item.dart
@@ -1,7 +1,6 @@
 import 'package:ccvc_mobile/generated/l10n.dart';
 import 'package:ccvc_mobile/presentation/calender_work/calender_work_day/mobile/calender_work_day_mobile.dart';
-import 'package:ccvc_mobile/presentation/chi_tiet_van_ban/ui/phone/chi_tiet_van_ban_screen.dart';
-import 'package:ccvc_mobile/presentation/chi_tiet_van_ban/ui/tablet/tablet.dart';
+import 'package:ccvc_mobile/presentation/calender_work/calender_work_day/tablet/calender_work_day_tablet.dart';
 import 'package:ccvc_mobile/presentation/home_screen/ui/mobile/home_screen.dart';
 import 'package:ccvc_mobile/presentation/home_screen/ui/tablet/home_screen_tablet.dart';
 import 'package:ccvc_mobile/presentation/menu_screen/ui/mobile/menu_screen.dart';
@@ -66,7 +65,10 @@ extension TabbarEnum on TabBarType {
           backgroundColor: Colors.blue,
         );
       case TabBarType.calendarWork:
-        return const CalenderWorkDayMobile();
+        return screenDevice(
+          mobileScreen: const CalenderWorkDayMobile(),
+          tabletScreen: const CalenderWorkDayTablet(),
+        );
       case TabBarType.internalInteraction:
         return const Scaffold(
           backgroundColor: Colors.cyanAccent,
@@ -76,7 +78,6 @@ extension TabbarEnum on TabBarType {
           mobileScreen: const MenuScreen(),
           tabletScreen: const MenuTabletScreen(),
         );
-
     }
   }
 
@@ -87,7 +88,6 @@ extension TabbarEnum on TabBarType {
           icon: SvgPicture.asset(
             isSelect ? ImageAssets.icHomeFocus : ImageAssets.icHomeUnFocus,
             height: 16,
-
           ),
           text: S.current.home,
         );
