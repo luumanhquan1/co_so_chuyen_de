b2af957595a387a363af083c418384d16918c689
DESKTOP-88FAOV3\EdsoLabs <>
2022-02-11 15:52:03 +0700

    fix bug quen mat khau

--- lib/presentation/reset_password/ui/tablet/send_mail_screen_tablet.dart
+++ lib/presentation/reset_password/ui/tablet/send_mail_screen_tablet.dart
@@ -5,8 +5,11 @@ import 'package:ccvc_mobile/presentation/forgot_password/bloc/forgot_password_cu
 import 'package:ccvc_mobile/presentation/login/ui/widgets/custom_textfield.dart';
 import 'package:ccvc_mobile/presentation/reset_password/ui/tablet/reset_password_screen_tablet.dart';
 import 'package:ccvc_mobile/utils/constants/image_asset.dart';
+import 'package:ccvc_mobile/utils/extensions/string_extension.dart';
 import 'package:ccvc_mobile/widgets/appbar/app_bar_default_back.dart';
 import 'package:ccvc_mobile/widgets/button/button_custom_bottom.dart';
+import 'package:ccvc_mobile/widgets/textformfield/form_group.dart';
+import 'package:ccvc_mobile/widgets/textformfield/text_field_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -22,64 +25,61 @@ class _SendMailScreenTabletState extends State<SendMailScreenTablet> {
   Widget build(BuildContext context) {
     final ForgotPasswordCubit cubit = ForgotPasswordCubit();
     final TextEditingController emailController = TextEditingController();
-    final keytextEmail = GlobalKey<FormState>();
+    final keyGroup = GlobalKey<FormGroupState>();
 
     return Scaffold(
+      backgroundColor: bgQLVBTablet,
       resizeToAvoidBottomInset: true,
       appBar: AppBarDefaultBack(S.current.dat_lai_mat_khau),
       body: SingleChildScrollView(
         child: Center(
           child: SizedBox(
             width: 510.0,
-            child: Column(
-              children: [
-                const SizedBox(height: 80.0),
-                Text(
-                  S.current.nhan_ma_xac_minh,
-                  style: textNormalCustom(color: titleColor, fontSize: 20.0),
-                ),
-                const SizedBox(height: 40.0),
-                Text(
-                  S.current.de_nhan_ma_xac_minh,
-                  style: textNormalCustom(
-                    fontSize: 16.0,
-                    color: titleColor,
-                    fontWeight: FontWeight.w400,
+            child: FormGroup(
+              key: keyGroup,
+              child: Column(
+                children: [
+                  const SizedBox(height: 80.0),
+                  Text(
+                    S.current.nhan_ma_xac_minh,
+                    style: textNormalCustom(color: titleColor, fontSize: 20.0),
                   ),
-                ),
-                const SizedBox(height: 24.0),
-                Form(
-                  key: keytextEmail,
-                  child: CustomTextField(
+                  const SizedBox(height: 40.0),
+                  Text(
+                    S.current.de_nhan_ma_xac_minh,
+                    style: textNormalCustom(
+                      fontSize: 16.0,
+                      color: titleColor,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  const SizedBox(height: 24.0),
+                  TextFieldValidator(
+                    fillColor: backgroundColorApp,
                     controller: emailController,
-                    isPass: true,
-                    textHint: S.current.email,
-                    prefixIcon: SvgPicture.asset(ImageAssets.ic_email),
-                    onChange: (text) {
-                      keytextEmail.currentState?.validate();
-                    },
-                    validate: (value) {
-                      return cubit.validateInputText(value!);
+                    hintText: S.current.email,
+                    prefixIcon: SizedBox(
+                      width: 20,
+                      height: 20,
+                      child: Center(
+                        child: SvgPicture.asset(ImageAssets.ic_email),),
+                    ),
+                    validator: (value) {
+                      return (value ?? '').checkEmail();
                     },
                   ),
-                ),
-                const SizedBox(height: 36.0),
-                ButtonCustomBottom(
-                  isColorBlue: false,
-                  title: S.current.tiep_theo,
-                  onPressed: () {
-                    keytextEmail.currentState?.validate();
-                    if (emailController.value.text.isNotEmpty) {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              const ResetPasswordScreenTablet(),
-                        ),
-                      );
-                    }
-                  },
-                ),
-              ],
+                  const SizedBox(height: 36.0),
+                  ButtonCustomBottom(
+                    isColorBlue: false,
+                    title: S.current.tiep_theo,
+                    onPressed: () {
+                      if (keyGroup.currentState!.validator()) {
+                        Navigator.of(context).pop();
+                      }
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),
