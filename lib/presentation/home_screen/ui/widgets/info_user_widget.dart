f8fff47adf6bb6462618fe7a142a8ac764739236
Lưu Mạnh Quân <macbook@Luu-Manh-Quan.local>
2022-02-16 17:52:10 +0700

    api home tong hop NV

--- lib/presentation/home_screen/ui/widgets/info_user_widget.dart
+++ lib/presentation/home_screen/ui/widgets/info_user_widget.dart
@@ -1,10 +1,12 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:ccvc_mobile/config/resources/color.dart';
 import 'package:ccvc_mobile/config/resources/styles.dart';
 import 'package:ccvc_mobile/config/themes/app_theme.dart';
 import 'package:ccvc_mobile/domain/model/account/data_user.dart';
-import 'package:ccvc_mobile/domain/model/user_infomation_model.dart';
+
 import 'package:ccvc_mobile/generated/l10n.dart';
 import 'package:ccvc_mobile/presentation/home_screen/ui/home_provider.dart';
+import 'package:ccvc_mobile/utils/constants/image_asset.dart';
 import 'package:ccvc_mobile/utils/extensions/size_extension.dart';
 import 'package:flutter/material.dart';
 
@@ -62,9 +64,16 @@ class InfoUserWidget extends StatelessWidget {
               Container(
                 height: 40.0.textScale(space: 8),
                 width: 40.0.textScale(space: 8),
+                clipBehavior: Clip.hardEdge,
                 decoration: const BoxDecoration(
                   shape: BoxShape.circle,
-                  color: Colors.red,
+                  color: Colors.transparent,
+                ),
+                child: CachedNetworkImage(
+                  imageUrl: result?.anhDaiDienFilePath ?? '',
+                  errorWidget: (context, url, error) => Container(
+                      color: Colors.black,
+                      child: Image.asset(ImageAssets.anhDaiDienMacDinh)),
                 ),
               )
             ],
