c65983316d2658403376fd2e1d2217973ce76147
Danghung301 <hung301le@gmail.com>
2022-02-18 13:17:32 +0700

    fix conflix

--- lib/data/di/di.dart
+++ lib/data/di/di.dart
@@ -1,8 +1,6 @@
 import 'package:get_it/get_it.dart';
-import 'package:injectable/injectable.dart';
-import 'di.config.dart';
+import 'di_config.dart';
 
 final GetIt getIt = GetIt.instance;
 
-@injectableInit
 void configureDependencies() => $initGetIt(getIt, environment: '');
