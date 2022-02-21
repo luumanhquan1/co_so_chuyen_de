4231b0747ec55602b817705eab0373bbaeedd97d
Danghung301 <hung301le@gmail.com>
2022-02-21 16:37:04 +0700

    lich su thu hoi

--- lib/presentation/chi_tiet_nhiem_vu/ui/phone/chi_tiet_nhiem_vu_phone_screen.dart
+++ lib/presentation/chi_tiet_nhiem_vu/ui/phone/chi_tiet_nhiem_vu_phone_screen.dart
@@ -3,6 +3,7 @@ import 'package:ccvc_mobile/domain/model/chi_tiet_nhiem_vu/chi_tiet_nhiem_vu_hea
 import 'package:ccvc_mobile/domain/model/chi_tiet_nhiem_vu/danh_sach_cong_viec.dart';
 import 'package:ccvc_mobile/domain/model/chi_tiet_nhiem_vu/lich_su_cap_nhat_thth.dart';
 import 'package:ccvc_mobile/domain/model/chi_tiet_nhiem_vu/lich_su_phan_xu_ly.dart';
+import 'package:ccvc_mobile/domain/model/chi_tiet_nhiem_vu/lich_su_thu_hoi.dart';
 import 'package:ccvc_mobile/domain/model/chi_tiet_nhiem_vu/lich_su_tra_lai.dart';
 import 'package:ccvc_mobile/domain/model/chi_tiet_nhiem_vu/van_ban_lien_quan.dart';
 import 'package:ccvc_mobile/generated/l10n.dart';
@@ -11,6 +12,7 @@ import 'package:ccvc_mobile/presentation/chi_tiet_nhiem_vu/ui/widget/danh_sach_c
 import 'package:ccvc_mobile/presentation/chi_tiet_nhiem_vu/ui/widget/header_chi_tiet.dart';
 import 'package:ccvc_mobile/presentation/chi_tiet_nhiem_vu/ui/widget/lich_su_cap_nhat_thth.dart';
 import 'package:ccvc_mobile/presentation/chi_tiet_nhiem_vu/ui/widget/lich_su_phan_xu_ly.dart';
+import 'package:ccvc_mobile/presentation/chi_tiet_nhiem_vu/ui/widget/lich_su_thu_hoi.dart';
 import 'package:ccvc_mobile/presentation/chi_tiet_nhiem_vu/ui/widget/lich_su_tra_lai.dart';
 import 'package:ccvc_mobile/presentation/chi_tiet_nhiem_vu/ui/widget/van_ban_lien_quan_widget.dart';
 import 'package:ccvc_mobile/widgets/appbar/base_app_bar.dart';
@@ -66,56 +68,66 @@ class _ChiTietNhiemVuPhoneScreenState extends State<ChiTietNhiemVuPhoneScreen> {
                 );
               },
             ),
-
             StreamBuilder<VanBanLienQuanModel>(
-                stream: cubit.vanBanLienQuanStream,
-                builder: (context, snapshot) {
-                  final data = snapshot.data ?? VanBanLienQuanModel.empty();
-                  return VanBanLienQuanWidget(
-                    dataModel: data,
-                    cubit: cubit,
-                  );
-                },),
-
+              stream: cubit.vanBanLienQuanStream,
+              builder: (context, snapshot) {
+                final data = snapshot.data ?? VanBanLienQuanModel.empty();
+                return VanBanLienQuanWidget(
+                  dataModel: data,
+                  cubit: cubit,
+                );
+              },
+            ),
             StreamBuilder<List<DanhSachCongViecModel>>(
-                stream: cubit.danhSachCongViecStream,
-                builder: (context, snapshot) {
-                  final data = snapshot.data ?? [];
-                  return DanhSachCongViecWidget(
-                    dataModel: data,
-                    cubit: cubit,
-                  );
-                },),
-
+              stream: cubit.danhSachCongViecStream,
+              builder: (context, snapshot) {
+                final data = snapshot.data ?? [];
+                return DanhSachCongViecWidget(
+                  dataModel: data,
+                  cubit: cubit,
+                );
+              },
+            ),
             StreamBuilder<List<LichSuPhanXuLyModel>>(
-                stream: cubit.lichSuPhanXuLySubject,
-                builder: (context, snapshot) {
-                  final data = snapshot.data ?? [];
-                  return LichSuPhanXuLyWidget(
-                    dataModel: data,
-                    cubit: cubit,
-                  );
-                },),
-
+              stream: cubit.lichSuPhanXuLySubject,
+              builder: (context, snapshot) {
+                final data = snapshot.data ?? [];
+                return LichSuPhanXuLyWidget(
+                  dataModel: data,
+                  cubit: cubit,
+                );
+              },
+            ),
             StreamBuilder<List<LichSuCapNhatTHTHModel>>(
-                stream: cubit.lichSuCapNhatTHTHModelStream,
-                builder: (context, snapshot) {
-                  final data = snapshot.data ?? [];
-                  return LichSuCapNhatTHTHWidget(
-                    dataModel: data,
-                    cubit: cubit,
-                  );
-                },),
-
+              stream: cubit.lichSuCapNhatTHTHModelStream,
+              builder: (context, snapshot) {
+                final data = snapshot.data ?? [];
+                return LichSuCapNhatTHTHWidget(
+                  dataModel: data,
+                  cubit: cubit,
+                );
+              },
+            ),
             StreamBuilder<List<LichSuTraLaiModel>>(
-                stream: cubit.lichSuTraLaiStream,
-                builder: (context, snapshot) {
-                  final data = snapshot.data ?? [];
-                  return LichSuTraLaiWidget(
-                    dataModel: data,
-                    cubit: cubit,
-                  );
-                },),
+              stream: cubit.lichSuTraLaiStream,
+              builder: (context, snapshot) {
+                final data = snapshot.data ?? [];
+                return LichSuTraLaiWidget(
+                  dataModel: data,
+                  cubit: cubit,
+                );
+              },
+            ),
+            StreamBuilder<List<LichSuThuHoiModel>>(
+              stream: cubit.lichSuThuHoiStream,
+              builder: (context, snapshot) {
+                final data = snapshot.data ?? [];
+                return LichSuThuHoiWidget(
+                  dataModel: data,
+                  cubit: cubit,
+                );
+              },
+            ),
           ],
         ),
       ),
