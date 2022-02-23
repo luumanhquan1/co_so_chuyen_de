ebd40aec0bb52f23613bca62fd6265dfecdd5962
Dang Xuan Tung <>
2022-02-23 17:22:13 +0700

    chuyen service

--- lib/domain/repository/chi_tiet_lich_lam_viec_repository/cancel_lich_lam_viec_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:ccvc_mobile/data/result/result.dart';
-import 'package:ccvc_mobile/domain/model/chi_tiet_lich_lam_viec/cancel_lich_lam_viec_model.dart';
-
-mixin CancelLichLamViecRepository {
-  Future<Result<CancelLichLamViecModel>> cancelCalenderWork(String id);
-}
