d135d3444bd80c86fed1b7988de2ee4a95b5e23f
HoaVu <hoavd@edsolabs.com>
2022-03-08 12:56:18 +0700

    fix_danh_sach vbdi, vb den thong tin chung_qlvb

---
+++ lib/domain/model/lich_hop/phien_hop_model.dart
@@ -0,0 +1,20 @@
+
+class PhienHopModel {
+  String? content;
+  String? id;
+  String? ngayTao;
+  String? nguoiTao;
+  String? nguoiTaoId;
+  String? scheduleId;
+  String? traLoiYKien;
+
+  PhienHopModel({
+    this.content,
+    this.id,
+    this.ngayTao,
+    this.nguoiTao,
+    this.nguoiTaoId,
+    this.scheduleId,
+    this.traLoiYKien,
+  });
+}
