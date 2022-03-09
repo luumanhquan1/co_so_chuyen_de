443a4f5175f58efa3ee6ef5f9fe342f477e641a3
HoaVu <hoavd@edsolabs.com>
2022-03-09 16:27:01 +0700

    bo comment man home

--- lib/presentation/tabbar_screen/ui/tabbar_item.dart
+++ lib/presentation/tabbar_screen/ui/tabbar_item.dart
@@ -56,14 +56,14 @@ extension TabbarEnum on TabBarType {
   Widget getScreen() {
     switch (this) {
       case TabBarType.home:
-        // return screenDevice(
-        //   mobileScreen: HomeScreenMobile(
-        //     key: keyHomeMobile,
-        //   ),
-        //   tabletScreen: HomeScreenTablet(
-        //     key: keyHomeTablet,
-        //   ),
-        // );
+        return screenDevice(
+          mobileScreen: HomeScreenMobile(
+            key: keyHomeMobile,
+          ),
+          tabletScreen: HomeScreenTablet(
+            key: keyHomeTablet,
+          ),
+        );
       case TabBarType.report:
         return screenDevice(
           mobileScreen: const DanhSachBaoCaoDangGirdviewMobile(),
