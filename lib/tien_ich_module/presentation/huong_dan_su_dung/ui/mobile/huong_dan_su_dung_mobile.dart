0ec36acf602d5f3ebde477c8051c9907786eb7e7
hungtv <>
2022-03-11 15:22:58 +0700

    api data topic huong dan su dung

--- lib/tien_ich_module/presentation/huong_dan_su_dung/ui/mobile/huong_dan_su_dung_mobile.dart
+++ lib/tien_ich_module/presentation/huong_dan_su_dung/ui/mobile/huong_dan_su_dung_mobile.dart
@@ -1,4 +1,5 @@
 import 'package:ccvc_mobile/generated/l10n.dart';
+import 'package:ccvc_mobile/tien_ich_module/presentation/huong_dan_su_dung/bloc/huong_dan_su_dung_cubit.dart';
 import 'package:ccvc_mobile/widgets/appbar/app_bar_default_back.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +11,13 @@ class HuongDanSuDungMobile extends StatefulWidget {
 }
 
 class _HuongDanSuDungMobileState extends State<HuongDanSuDungMobile> {
+  late HuongDanSuDungCubit cubit;
+  @override
+  void initState() {
+    cubit=HuongDanSuDungCubit();
+    cubit.getTopicHDSD();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
