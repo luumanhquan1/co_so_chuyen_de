bd372312299eefbdcffc9794f5aef5512ef90f16
Dang Xuan Tung <dangxuantung@MacbookOfDucTung.local>
2022-03-28 09:05:49 +0700

    fix hard code

--- lib/domain/model/lich_hop/chuong_trinh_hop.dart
+++ lib/domain/model/lich_hop/chuong_trinh_hop.dart
@@ -1,4 +1,8 @@
+import 'package:ccvc_mobile/config/resources/color.dart';
 import 'package:ccvc_mobile/data/response/lich_hop/danh_sach_can_bo_response.dart';
+import 'package:ccvc_mobile/generated/l10n.dart';
+import 'package:ccvc_mobile/presentation/chi_tiet_lich_hop/bloc/chi_tiet_lich_hop_cubit.dart';
+import 'package:flutter/cupertino.dart';
 
 class ChuongTrinhHopModel {
   List<CanBoModel>? listCanBo;
@@ -55,4 +59,56 @@ class CanBoModel {
     required this.isThamGiaBocBang,
     required this.createAt,
   });
+}
+
+extension trangthaiAndDiemDanhTPTG on DetailMeetCalenderCubit {
+  String trangThai(int? tt) {
+    switch (tt) {
+      case 1:
+        return S.current.tham_gia;
+      case 2:
+        return S.current.tu_choi_tham_du;
+      case 4:
+        return S.current.tham_du;
+      default:
+        return S.current.cho_xac_nhan;
+    }
+  }
+
+  Color trangThaiColor(int? tt) {
+    switch (tt) {
+      case 1:
+        return daXuLyColor;
+      case 2:
+        return choXuLyColor;
+      case 4:
+        return statusCalenderRed;
+      default:
+        return itemWidgetNotUse;
+    }
+  }
+
+  String diemDanh(bool? isVM, bool? dD) {
+    if (isVM == true) {
+      return S.current.vang_mat;
+    } else if (isVM == false || dD == true) {
+      return S.current.da_diem_danh;
+    } else if (isVM == false || dD == false) {
+      return S.current.chua_diem_danh;
+    } else {
+      return '';
+    }
+  }
+
+  Color diemDanhColors(bool? isVM, bool? dD) {
+    if (isVM == true) {
+      return infoColor;
+    } else if (isVM == false || dD == true) {
+      return deliveredColor;
+    } else if (isVM == false || dD == false) {
+      return statusCalenderRed;
+    } else {
+      return backgroundColorApp;
+    }
+  }
 }
