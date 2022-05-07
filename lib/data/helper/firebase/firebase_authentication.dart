77c26e31f97e4cd7e94b97e108ab30c28b277ca2
hung301 <hung301le@gmail.com>
2022-05-07 22:39:54 +0700

    login logic

--- lib/data/helper/firebase/firebase_authentication.dart
+++ lib/data/helper/firebase/firebase_authentication.dart
@@ -9,20 +9,16 @@ class FirebaseAuthentication {
   static Future<User?> signInUsingEmailPassword({
     required String email,
     required String password,
-    required BuildContext context,
   }) async {
     User? user;
 
     try {
-      print('--------------');
       final UserCredential userCredential =
           await auth.signInWithEmailAndPassword(
         email: email,
         password: password,
       );
-      print('11111111111111111');
       user = userCredential.user;
-      print('2222222222222222222');
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
         print('No user found for that email.');
--- lib/presentation/login/bloc/login_cubit.dart
+++ lib/presentation/login/bloc/login_cubit.dart
@@ -1,6 +1,8 @@
-
 import 'package:ccvc_mobile/config/base/base_cubit.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:ccvc_mobile/data/helper/firebase/firebase_authentication.dart';
+import 'package:ccvc_mobile/domain/locals/hive_local.dart';
+import 'package:ccvc_mobile/domain/model/login/user_info.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'login_state.dart';
 
@@ -12,10 +14,35 @@ class LoginCubit extends BaseCubit<LoginState> {
   bool isHideEye1 = false;
   bool passIsError = false;
 
-  
+  Future<User?> lognIn(
+    String email,
+    String password,
+  ) async {
+    showLoading();
+    final User? user = await FirebaseAuthentication.signInUsingEmailPassword(
+      email: email,
+      password: password,
+    );
+
+    if (user != null) {
+      final UserInfoModel dataUser = UserInfoModel(
+        userId: user.uid,
+        avataUrl: user.photoURL,
+        email: user.email,
+        birthday: user.email,
+        gender: true,
+        nameDisplay: user.displayName,
+        createAt: '',
+        updateAt: '',
+      );
+
+      HiveLocal.saveDataUser(dataUser);
+    }
+    showContent();
+    return user;
+  }
 
   void closeDialog() {
     showContent();
   }
-
 }
